1e58fd01b231ffd02cee16071b1a0d29d19f5330
newton-w <71337527+newton-w@users.noreply.github.com>
2023-03-02 14:43:05 +0300

    update

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -1,27 +1,19 @@
 import React from 'react'
 import personMobile from '../images/mobile/image-interactive.jpg'
 import personDesktop from '../images/desktop/image-interactive.jpg'
-import { motion } from 'framer-motion'
+
 
 const About = () => {
     return (
         <div className='flex flex-col items-center justify-center px-6 py-24 lg:flex-row xl:container xl:mx-auto'>
-            <motion.picture
-                initial={{ x: -500 }}
-                whileInView={{ x: 0 }}
-                transition={{ type: "spring", stiffness: 70 }}
-                viewport={{once : true}}
-
-            >
+            <picture>
                 <source media="(min-width: 50rem)" srcSet={personDesktop} />
                 <img src={personMobile} alt="A person wearing vr headsets playing a game" />
-            </motion.picture>
-            <motion.div
-            
-            className=' flex flex-col gap-7 px-7 text-center lg:w-[40.625rem] lg:h-[25.313rem] lg:bg-white lg:justify-center lg:items-start lg:p-24 lg:translate-x-[-11rem] lg:translate-y-40 lg:mb-24'>
+            </picture>
+            <div className=' flex flex-col gap-7 px-7 text-center lg:w-[40.625rem] lg:h-[25.313rem] lg:bg-white lg:justify-center lg:items-start lg:p-24 lg:translate-x-[-11rem] lg:translate-y-40 lg:mb-24'>
                 <h1 className=' font-JosefinSans text-2xl pt-12 lg:pt-0 lg:text-4xl lg:text-start'>THE LEADER IN INTERACTIVE VR</h1>
                 <p className=' font-Alata text-base text-VeryDarkGrey lg:text-start '>Founded in 2011, Loopstudios has been producing world-class virtual reality projects for some of the best companies around the globe. Our award-winning creations have transformed businesses through digital experiences that bind their brand.</p>
-            </motion.div>
+            </div>
         </div>
     )
 }
